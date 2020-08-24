e83116cd43991092832f637e48c715446069b03a
albertsundjaja <albertsundjaja@gmail.com>
2020-08-25 00:16:12 +1000

    checkpoint

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,27 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+import * as AWS  from 'aws-sdk'
 
-//   // TODO: Remove a TODO item by id
-//   return undefined
-// }
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+
+  await docClient.delete({
+      TableName: todosTable,
+      Key: {
+          todoId
+      }
+  }).promise()
+  // TODO: Remove a TODO item by id
+  return {
+    statusCode: 202,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: null
+  }
+}
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,45 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+import * as AWS  from 'aws-sdk'
 
-//   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-//   return undefined
-// }
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+
+  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+  await docClient.update({
+      TableName: todosTable,
+      Key: {
+          todoId
+      },
+      AttributeUpdates: {
+          name: {
+              Action: 'PUT',
+              Value: updatedTodo.name
+          },
+          dueDate: {
+              Action: 'PUT',
+              Value: updatedTodo.dueDate
+          },
+          done: {
+              Action: 'PUT',
+              Value: updatedTodo.done
+          }
+      }
+  }).promise()
+  
+  return {
+    statusCode: 202,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: null
+  }
+}
